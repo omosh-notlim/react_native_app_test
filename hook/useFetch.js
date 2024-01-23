aa98cfca22742a2c2c04e92be9cc1e39b8fa2d61
Milton Omondi <klsictintern1@siginon.com>
2024-01-23 11:10:37 +0300

    This is a simple react native app that fetches data from an api related to job vaccancies

---
+++ hook/useFetch.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import { RAPID_API_KEY } from '@env';
+
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': '8986c4ecb0mshe2de9ab87b68db4p1a069cjsn626e8b72bc8f',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: {
+            // query: 'java',
+            // page '1',
+            // num_pages: '1'
+            ...query
+        },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('An error occured');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
